2620fe8c99f310d0c2358f533581ca8834ca0423
RounakGhosal <rounakghosal11@gmail.com>
2026-03-31 22:10:48 +0530

    first commit

--- app.py
+++ app.py
@@ -4,19 +4,22 @@ app.secret_key = "abc"
 
 @app.route("/")
 def home():
+    if "user" in session:
+        return render_template("index.html", username=session["user"])
     return render_template("index.html")
 
 @app.route("/link")
 def link():
-    return render_template("link.html")
+    if "user" not in session:
+        return "Please login first"
+    return render_template("link.html", user=session["user"])
 
 @app.route("/submit", methods=["POST"])
 def submit():
     username = request.form["username"]
     password = request.form["password"]
     session["user"] = username
-    session["pass"] = password
-    return render_template("index.html", username=username, password=password)
+    return render_template("index.html", username=username)
 
 if __name__ == "__main__":
     app.run(debug=True)
