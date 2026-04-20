b644573e0ffc81a484693d52b3351324811663ff
RounakGhosal <rounakghosal11@gmail.com>
2026-04-20 13:26:04 +0530

    first commit

--- app.py
+++ app.py
@@ -1,42 +1,15 @@
 from flask import Flask, render_template, url_for, request, session, flash, redirect
 from datetime import timedelta
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = "abc"
 app.permanent_session_lifetime = timedelta(minutes=3)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
 
 @app.route("/")
 def home():
     if "user" in session:
         return render_template("index.html", username=session["user"])
     return render_template("index.html")
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        session.permanent = True
-
-        username = request.form["username"]
-        password = generate_password_hash(request.form["password"])
-        new_user = User(username=username, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        flash("Registered successfully", "success")
-        return redirect(url_for("user"))   
-
-    return render_template("register.html")  
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -45,14 +18,11 @@ def login():
 
         username = request.form["username"]
         password = request.form["password"]
-        user = User.query.filter_by(username=username).first()
-        if user and check_password_hash(user.password,password):
-            flash("Logged in successfully", "success")
-            session["user"] = username
-            return redirect(url_for("user"))  
-        else:
-            flash("Invalid credentials","error")
-            return redirect(url_for("login"))
+
+        session["user"] = username
+
+        flash("Logged in successfully", "success")
+        return redirect(url_for("user"))   
 
     return render_template("login.html")   
 
@@ -82,6 +52,4 @@ def logout():
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
